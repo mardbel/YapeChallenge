a6ee7da1f6267fedb848222815c5d2f36a139b1c
Martin Del Bel <martin.delbel@soytul.com>
2023-01-28 21:10:23 -0300

    add receipt service

--- app/src/main/java/com/example/yapechallenge/presentation/MainActivity.kt
+++ app/src/main/java/com/example/yapechallenge/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.yapechallenge
+package com.example.yapechallenge.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.yapechallenge.R
 import com.example.yapechallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
---
+++ app/src/main/java/model/Receipt.kt
@@ -0,0 +1,9 @@
+package model
+
+data class Receipt(
+    val name: String,
+    val id: String,
+    val latitude: Int,
+    val longitude: Int,
+    val image: String
+)
